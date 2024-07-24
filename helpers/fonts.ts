b5b29b0d301f069e0feb947e8df3ba5d21677637
fleven <franz.leven@kickstartds.com>
2024-07-24 09:40:36 +0200

    feat(design): design application

--- helpers/fonts.ts
+++ helpers/fonts.ts
@@ -3,53 +3,63 @@ import localFont from "next/font/local";
 const displayFont = localFont({
   src: [
     {
-      path: "../token/fonts/Fredoka-Regular.woff2",
+      path: "../token/fonts/Metropolis-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Regular.woff",
+      path: "../token/fonts/Metropolis-Regular.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-SemiBold.woff2",
+      path: "../token/fonts/Metropolis-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-SemiBold.woff",
+      path: "../token/fonts/Metropolis-SemiBold.woff",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Medium.woff2",
+      path: "../token/fonts/Metropolis-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Medium.woff",
+      path: "../token/fonts/Metropolis-Medium.woff",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Light.woff2",
+      path: "../token/fonts/Metropolis-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Light.woff",
+      path: "../token/fonts/Metropolis-Light.woff",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Bold.woff2",
+      path: "../token/fonts/Metropolis-Bold.woff2",
       weight: "bold",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Bold.woff",
+      path: "../token/fonts/Metropolis-Bold.woff",
       weight: "bold",
+      style: "normal",
+    },
+    {
+      path: "../token/fonts/Metropolis-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../token/fonts/Metropolis-Black.woff",
+      weight: "900",
       style: "normal",
     },
   ],
@@ -166,52 +176,52 @@ const interfaceFont = localFont({
 const displayFontPreview = localFont({
   src: [
     {
-      path: "../token/fonts/Fredoka-Regular.woff2",
+      path: "../token/fonts/Metropolis-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Regular.woff",
+      path: "../token/fonts/Metropolis-Regular.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-SemiBold.woff2",
+      path: "../token/fonts/Metropolis-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-SemiBold.woff",
+      path: "../token/fonts/Metropolis-SemiBold.woff",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Medium.woff2",
+      path: "../token/fonts/Metropolis-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Medium.woff",
+      path: "../token/fonts/Metropolis-Medium.woff",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Light.woff2",
+      path: "../token/fonts/Metropolis-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Light.woff",
+      path: "../token/fonts/Metropolis-Light.woff",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Bold.woff2",
+      path: "../token/fonts/Metropolis-Bold.woff2",
       weight: "bold",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Bold.woff",
+      path: "../token/fonts/Metropolis-Bold.woff",
       weight: "bold",
       style: "normal",
     },
