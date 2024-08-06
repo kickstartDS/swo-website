607989e6e0b51e287464910e3652098905aac66c
fleven <franz.leven@kickstartds.com>
2024-08-06 11:04:42 +0200

    feat(various): style application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,9 +46,20 @@ export default function App({
   Component: NextPage;
 }) {
   const { settings, story, blurHashes } = pageProps;
-  const headerProps = settings?.header[0];
-  const footerProps = settings?.footer[0];
+  const headerProps = settings?.header[0] ? unflatten(settings?.header[0]) : {};
+  const footerProps = settings?.footer[0] ? unflatten(settings?.footer[0]) : {};
+  const storyProps = story?.content ? unflatten(story?.content) : {};
   const router = useRouter();
+
+  const invertHeader = storyProps?.header?.inverted
+    ? !headerProps?.inverted || true
+    : headerProps?.inverted || false;
+  const floatHeader = storyProps?.header?.floating
+    ? !headerProps?.floating || true
+    : headerProps?.floating || false;
+  const invertFooter = storyProps?.footer?.inverted
+    ? !footerProps?.inverted || true
+    : footerProps?.inverted || false;
 
   setActiveNavItem(headerProps?.navItems, router.asPath);
   setActiveNavItem(footerProps?.navItems, router.asPath);
@@ -57,8 +68,6 @@ export default function App({
     router.events.on("routeChangeStart", handleRouteChange);
     return () => router.events.off("routeChangeStart", handleRouteChange);
   }, [router.events]);
-
-  console.log(story);
 
   return (
     <BlurHashProvider blurHashes={blurHashes}>
@@ -73,18 +82,14 @@ export default function App({
           {headerProps && (
             <Header
               logo={{}}
-              {...unflatten(headerProps)}
-              inverted={story.content.header_inverted.toString() || false}
-              floating={story.content.header_floating.toString() || false}
+              {...headerProps}
+              inverted={invertHeader}
+              floating={floatHeader}
             />
           )}
           <Component {...pageProps} />
           {footerProps && (
-            <Footer
-              logo={{}}
-              {...unflatten(footerProps)}
-              inverted={story.content?.footer_inverted.toString() || false}
-            />
+            <Footer logo={{}} {...footerProps} inverted={invertFooter} />
           )}
         </StoryblokProviders>
       </DsaProviders>
