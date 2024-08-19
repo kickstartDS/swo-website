0784754075e1fe398399101c56dc0fc17444106c
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2024-08-19 14:38:30 +0200

    fix: empty images reserving visual space after deletion

--- components/StoryblokSubComponent.tsx
+++ components/StoryblokSubComponent.tsx
@@ -1,5 +1,6 @@
 import { unflatten } from "@/helpers/unflatten";
 import { StoryblokComponent } from "@storyblok/react";
+import { traverse } from "object-traversal";
 import { forwardRef } from "react";
 
 const isStoryblokComponent = (
@@ -8,9 +9,24 @@ const isStoryblokComponent = (
   blok.content !== undefined && blok.id !== undefined;
 
 // eslint-disable-next-line react/display-name
-export const StoryblokSubComponent = forwardRef<HTMLElement>((props, ref) => (
-  <StoryblokComponent
-    blok={unflatten(isStoryblokComponent(props) ? props.content : props)}
-    ref={ref}
-  />
-));
+export const StoryblokSubComponent = forwardRef<HTMLElement>((props, ref) => {
+  traverse(props, ({ parent, key, value }) => {
+    if (
+      parent &&
+      key &&
+      value &&
+      typeof value === "object" &&
+      value.fieldtype === "asset" &&
+      value.id === null
+    ) {
+      delete parent[key];
+    }
+  });
+  console.log(props);
+  return (
+    <StoryblokComponent
+      blok={unflatten(isStoryblokComponent(props) ? props.content : props)}
+      ref={ref}
+    />
+  );
+});
