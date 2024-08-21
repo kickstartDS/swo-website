2157795440eaf750c8319c3ad4fe4f018a1b884d
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2024-08-21 11:55:53 +0200

    fix: update fonts

--- helpers/fonts.ts
+++ helpers/fonts.ts
@@ -40,103 +40,6 @@ const displayFont = localFont({
   adjustFontFallback: false,
 });
 
-const copyFont = localFont({
-  src: [
-    {
-      path: "../token/fonts/Mulish-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Mulish-Regular.woff",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Mulish-600.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-600.woff", weight: "600", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-800.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-800.woff", weight: "800", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-300.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-300.woff", weight: "300", style: "normal" },
-  ],
-  preload: true,
-  display: "swap",
-  variable: "--ks-font-family-copy",
-  fallback: ["'Helvetica Neue'", "Helvetica", "Arial", "sans-serif"],
-  adjustFontFallback: false,
-});
-
-const interfaceFont = localFont({
-  src: [
-    {
-      path: "../token/fonts/Mulish-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Mulish-Regular.woff",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Mulish-600.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-600.woff", weight: "600", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-800.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-800.woff", weight: "800", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-300.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-300.woff", weight: "300", style: "normal" },
-  ],
-  preload: true,
-  display: "optional",
-  variable: "--ks-font-family-interface",
-  fallback: [
-    "system-ui",
-    "-apple-system",
-    "BlinkMacSystemFont",
-    "Avenir Next",
-    "Avenir",
-    "Segoe UI",
-    "Lucida Grande",
-    "Helvetica Neue",
-    "Helvetica",
-    "Fira Sans",
-    "Roboto",
-    "Noto",
-    "Droid Sans",
-    "Cantarell",
-    "Oxygen",
-    "Ubuntu",
-    "Franklin Gothic Medium",
-    "Century Gothic",
-    "Liberation Sans",
-    "sans-serif",
-  ],
-  adjustFontFallback: false,
-});
-
 const displayFontPreview = localFont({
   src: [
     {
@@ -170,23 +73,23 @@ const displayFontPreview = localFont({
       style: "normal",
     },
     {
-      path: "../token/fonts/Metropolis-Light.woff2",
-      weight: "300",
+      path: "../token/fonts/metropolis-thin.woff2",
+      weight: "100",
       style: "normal",
     },
     {
-      path: "../token/fonts/Metropolis-Light.woff",
-      weight: "300",
+      path: "../token/fonts/metropolis-thin.woff",
+      weight: "100",
       style: "normal",
     },
     {
       path: "../token/fonts/Metropolis-Bold.woff2",
-      weight: "bold",
+      weight: "700",
       style: "normal",
     },
     {
       path: "../token/fonts/Metropolis-Bold.woff",
-      weight: "bold",
+      weight: "700",
       style: "normal",
     },
   ],
@@ -203,102 +106,5 @@ const displayFontPreview = localFont({
   adjustFontFallback: false,
 });
 
-const copyFontPreview = localFont({
-  src: [
-    {
-      path: "../token/fonts/Mulish-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Mulish-Regular.woff",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Mulish-600.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-600.woff", weight: "600", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-800.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-800.woff", weight: "800", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-300.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-300.woff", weight: "300", style: "normal" },
-  ],
-  preload: false,
-  display: "auto",
-  variable: "--ks-font-family-copy",
-  fallback: ["'Helvetica Neue'", "Helvetica", "Arial", "sans-serif"],
-  adjustFontFallback: false,
-});
-
-const interfaceFontPreview = localFont({
-  src: [
-    {
-      path: "../token/fonts/Mulish-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Mulish-Regular.woff",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Mulish-600.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-600.woff", weight: "600", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-800.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-800.woff", weight: "800", style: "normal" },
-    {
-      path: "../token/fonts/Mulish-300.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    { path: "../token/fonts/Mulish-300.woff", weight: "300", style: "normal" },
-  ],
-  preload: false,
-  display: "auto",
-  variable: "--ks-font-family-interface",
-  fallback: [
-    "system-ui",
-    "-apple-system",
-    "BlinkMacSystemFont",
-    "Avenir Next",
-    "Avenir",
-    "Segoe UI",
-    "Lucida Grande",
-    "Helvetica Neue",
-    "Helvetica",
-    "Fira Sans",
-    "Roboto",
-    "Noto",
-    "Droid Sans",
-    "Cantarell",
-    "Oxygen",
-    "Ubuntu",
-    "Franklin Gothic Medium",
-    "Century Gothic",
-    "Liberation Sans",
-    "sans-serif",
-  ],
-  adjustFontFallback: false,
-});
-
-export const fontClassNames = `${displayFont.variable} ${copyFont.variable} ${interfaceFont.variable}`;
-export const fontClassNamesPreview = `${displayFontPreview.variable} ${copyFontPreview.variable} ${interfaceFontPreview.variable}`;
+export const fontClassNames = `${displayFont.variable}`;
+export const fontClassNamesPreview = `${displayFontPreview.variable}`;
