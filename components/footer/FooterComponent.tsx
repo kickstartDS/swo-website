090aeebffbb6a99b755f6a95c3f5a64b1c044957
fleven <franz.leven@kickstartds.com>
2024-09-11 14:51:53 +0200

    feat(footer): customize footer

--- components/footer/FooterComponent.tsx
+++ components/footer/FooterComponent.tsx
@@ -17,7 +17,7 @@ import { Icon } from "@kickstartds/base/lib/icon";
 export const FooterContextDefault = forwardRef<
   HTMLDivElement,
   ComponentProps<typeof DsaFooter> & HTMLAttributes<HTMLDivElement>
->(({ navItems, byline, inverted, logo }, ref) =>
+>(({ navItems, inverted, logo }, ref) =>
   navItems && navItems.length > 0 ? (
     <div
       className={classnames("dsa-footer")}
@@ -25,43 +25,46 @@ export const FooterContextDefault = forwardRef<
       ref={ref}
     >
       <div className="dsa-footer__content dsa-footer__content--top">
-        <address className="dsa-footer__address">
-          <span className="dsa-footer__address__item">ruhmesmeile GmbH</span>
-          <Link
-            className="dsa-footer__address__item dsa-footer__address__link"
-            href="tel:+49 228 30412660"
-          >
-            +49 228 30412660
-          </Link>
-          <Link
-            className="dsa-footer__address__item dsa-footer__address__link"
-            href="mailto:mail@ruhmesmeile.com"
-          >
-            mail@ruhmesmeile.com
-          </Link>
-          <span className="dsa-footer__address__item">Mozartstraße 4 - 10</span>
-          <span className="dsa-footer__address__item">53115 Bonn</span>
+        <address className="dsa-footer__column dsa-footer__adress">
+          <ul className="dsa-footer__contact">
+            <Link className="dsa-footer__headline" href="tel:+49 228 30412660">
+              +49 228 30412660
+            </Link>
+            <Link
+              className="dsa-footer__headline"
+              href="mailto:mail@ruhmesmeile.com"
+            >
+              mail@ruhmesmeile.com
+            </Link>
+          </ul>
+          <span className="dsa-footer__item">ruhmesmeile GmbH</span>
+          <span className="dsa-footer__item">Mozartstraße 4 - 10</span>
+          <span className="dsa-footer__item">53115 Bonn</span>
         </address>
 
-        {navItems.length > 0 ? (
-          <ul className="dsa-footer__links dsa-footer__links--top">
-            {navItems.map(({ label, active, ...linkProps }) => (
-              <Link
-                {...linkProps}
-                className="dsa-footer__link"
-                key={linkProps.href + label}
-              >
-                {label}
-              </Link>
-            ))}
+        <nav className="dsa-footer__links dsa-footer__links--top">
+          <ul className="dsa-footer__column">
+            <span className="dsa-footer__headline">Design System Services</span>
+            <Link className="dsa-footer__link">Beratung</Link>
+            <Link className="dsa-footer__link">Design System Training</Link>
+            <Link className="dsa-footer__link">UX-Strategie</Link>
           </ul>
-        ) : null}
+          <ul className="dsa-footer__column">
+            <span className="dsa-footer__headline">Headless CMS</span>
+            <Link className="dsa-footer__link">Wissenswertes</Link>
+            <Link className="dsa-footer__link">CMS Starter</Link>
+            <Link className="dsa-footer__link">Storyblok Agentur</Link>
+          </ul>
+          <ul className="dsa-footer__column">
+            <span className="dsa-footer__headline">Agentur & Beratung</span>
+            <Link className="dsa-footer__link">Über uns</Link>
+            <Link className="dsa-footer__link">Insights</Link>
+            <Link className="dsa-footer__link">Kontakt</Link>
+          </ul>
+        </nav>
       </div>
       <div className="dsa-footer__content dsa-footer__content--bottom">
         <ul className="dsa-footer__links dsa-footer__links--bottom">
-          <Link className="dsa-footer__link" href="ueber-uns/kontakt">
-            Kontakt
-          </Link>
           <Link className="dsa-footer__link" href="/impressum">
             Impressum
           </Link>
