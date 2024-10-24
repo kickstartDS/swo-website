1dd9ae542ca5b112e8d22f76b78483e672a82d06
fleven <franz.leven@kickstartds.com>
2024-10-24 14:57:01 +0200

    feat(token): set up token

--- helpers/fonts.ts
+++ helpers/fonts.ts
@@ -3,53 +3,13 @@ import localFont from "next/font/local";
 const displayFont = localFont({
   src: [
     {
-      path: "../token/fonts/Fredoka-Regular.woff2",
+      path: "../token/fonts/VollkornSC-SemiBold.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Regular.woff",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-SemiBold.woff2",
+      path: "../token/fonts/VollkornSC-SemiBold.woff",
       weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-SemiBold.woff",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Medium.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Medium.woff",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Light.woff",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Bold.woff",
-      weight: "700",
       style: "normal",
     },
   ],
@@ -69,33 +29,45 @@ const displayFont = localFont({
 const copyFont = localFont({
   src: [
     {
-      path: "../token/fonts/Mulish-Regular.woff2",
+      path: "../token/fonts/NotoSans-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Mulish-Regular.woff",
+      path: "../token/fonts/NotoSans-Regular.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Mulish-600.woff2",
+      path: "../token/fonts/NotoSans-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
-    { path: "../token/fonts/Mulish-600.woff", weight: "600", style: "normal" },
     {
-      path: "../token/fonts/Mulish-800.woff2",
-      weight: "800",
+      path: "../token/fonts/NotoSans-SemiBold.woff",
+      weight: "600",
       style: "normal",
     },
-    { path: "../token/fonts/Mulish-800.woff", weight: "800", style: "normal" },
     {
-      path: "../token/fonts/Mulish-300.woff2",
+      path: "../token/fonts/NotoSans-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../token/fonts/NotoSans-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../token/fonts/NotoSans-Light.woff2",
       weight: "300",
       style: "normal",
     },
-    { path: "../token/fonts/Mulish-300.woff", weight: "300", style: "normal" },
+    {
+      path: "../token/fonts/NotoSans-Light.woff",
+      weight: "300",
+      style: "normal",
+    },
   ],
   preload: true,
   display: "swap",
@@ -107,33 +79,45 @@ const copyFont = localFont({
 const interfaceFont = localFont({
   src: [
     {
-      path: "../token/fonts/Mulish-Regular.woff2",
+      path: "../token/fonts/NotoSans-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Mulish-Regular.woff",
+      path: "../token/fonts/NotoSans-Regular.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Mulish-600.woff2",
+      path: "../token/fonts/NotoSans-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
-    { path: "../token/fonts/Mulish-600.woff", weight: "600", style: "normal" },
     {
-      path: "../token/fonts/Mulish-800.woff2",
-      weight: "800",
+      path: "../token/fonts/NotoSans-SemiBold.woff",
+      weight: "600",
       style: "normal",
     },
-    { path: "../token/fonts/Mulish-800.woff", weight: "800", style: "normal" },
     {
-      path: "../token/fonts/Mulish-300.woff2",
+      path: "../token/fonts/NotoSans-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../token/fonts/NotoSans-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../token/fonts/NotoSans-Light.woff2",
       weight: "300",
       style: "normal",
     },
-    { path: "../token/fonts/Mulish-300.woff", weight: "300", style: "normal" },
+    {
+      path: "../token/fonts/NotoSans-Light.woff",
+      weight: "300",
+      style: "normal",
+    },
   ],
   preload: true,
   display: "optional",
@@ -166,53 +150,13 @@ const interfaceFont = localFont({
 const displayFontPreview = localFont({
   src: [
     {
-      path: "../token/fonts/Fredoka-Regular.woff2",
+      path: "../token/fonts/VollkornSC-SemiBold.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Fredoka-Regular.woff",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-SemiBold.woff2",
+      path: "../token/fonts/VollkornSC-SemiBold.woff",
       weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-SemiBold.woff",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Medium.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Medium.woff",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Light.woff",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../token/fonts/Fredoka-Bold.woff",
-      weight: "700",
       style: "normal",
     },
   ],
@@ -232,33 +176,45 @@ const displayFontPreview = localFont({
 const copyFontPreview = localFont({
   src: [
     {
-      path: "../token/fonts/Mulish-Regular.woff2",
+      path: "../token/fonts/NotoSans-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Mulish-Regular.woff",
+      path: "../token/fonts/NotoSans-Regular.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Mulish-600.woff2",
+      path: "../token/fonts/NotoSans-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
-    { path: "../token/fonts/Mulish-600.woff", weight: "600", style: "normal" },
     {
-      path: "../token/fonts/Mulish-800.woff2",
-      weight: "800",
+      path: "../token/fonts/NotoSans-SemiBold.woff",
+      weight: "600",
       style: "normal",
     },
-    { path: "../token/fonts/Mulish-800.woff", weight: "800", style: "normal" },
     {
-      path: "../token/fonts/Mulish-300.woff2",
+      path: "../token/fonts/NotoSans-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../token/fonts/NotoSans-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../token/fonts/NotoSans-Light.woff2",
       weight: "300",
       style: "normal",
     },
-    { path: "../token/fonts/Mulish-300.woff", weight: "300", style: "normal" },
+    {
+      path: "../token/fonts/NotoSans-Light.woff",
+      weight: "300",
+      style: "normal",
+    },
   ],
   preload: false,
   display: "auto",
@@ -270,33 +226,45 @@ const copyFontPreview = localFont({
 const interfaceFontPreview = localFont({
   src: [
     {
-      path: "../token/fonts/Mulish-Regular.woff2",
+      path: "../token/fonts/NotoSans-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Mulish-Regular.woff",
+      path: "../token/fonts/NotoSans-Regular.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../token/fonts/Mulish-600.woff2",
+      path: "../token/fonts/NotoSans-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
-    { path: "../token/fonts/Mulish-600.woff", weight: "600", style: "normal" },
     {
-      path: "../token/fonts/Mulish-800.woff2",
-      weight: "800",
+      path: "../token/fonts/NotoSans-SemiBold.woff",
+      weight: "600",
       style: "normal",
     },
-    { path: "../token/fonts/Mulish-800.woff", weight: "800", style: "normal" },
     {
-      path: "../token/fonts/Mulish-300.woff2",
+      path: "../token/fonts/NotoSans-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../token/fonts/NotoSans-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../token/fonts/NotoSans-Light.woff2",
       weight: "300",
       style: "normal",
     },
-    { path: "../token/fonts/Mulish-300.woff", weight: "300", style: "normal" },
+    {
+      path: "../token/fonts/NotoSans-Light.woff",
+      weight: "300",
+      style: "normal",
+    },
   ],
   preload: false,
   display: "auto",
